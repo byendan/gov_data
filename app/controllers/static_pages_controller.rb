1c5de73d4e98affc9f253e01846addae2ceab119
byendan <byendanb@gmail.com>
2016-07-13 14:28:26 -0700

    Hid gov api key. Made a basic implementation of my api using module. Applied it to using the nasa image of the day to start off a nice looking home page.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,13 @@
 class StaticPagesController < ApplicationController
+  include GovApi
+
   def show
+    # sets up nasa image of day
+    nasa_data_search = ['explanation', 'hdurl', 'title']
+    nasa_image_data = get_data("https://api.nasa.gov/planetary/apod", nasa_data_search)
+    @nasa_image_title = nasa_image_data['title']
+    @nasa_hd_image = nasa_image_data['hdurl']
+    @nasa_image_explanation = nasa_image_data['explanation']
+    @nasa_media_type = nasa_image_data['media_type']
   end
 end
