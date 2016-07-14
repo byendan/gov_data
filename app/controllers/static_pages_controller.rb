f05d5bde2121de47e5137a671c431c7676394253
byendan <byendanb@gmail.com>
2016-07-14 15:00:19 -0700

    Wrote test to confirm the api data can be used correctly for the nasa apod api.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,7 @@ class StaticPagesController < ApplicationController
   def show
     # sets up nasa image of day
     nasa_data_search = ['explanation', 'hdurl', 'title']
-    nasa_image_data = get_data("https://api.nasa.gov/planetary/apod", nasa_data_search)
+    nasa_image_data = get_data(api_respond_json("https://api.nasa.gov/planetary/apod"), nasa_data_search)
     @nasa_image_title = nasa_image_data['title']
     @nasa_hd_image = nasa_image_data['hdurl']
     @nasa_image_explanation = nasa_image_data['explanation']
