700ff7c023cda94c3e3abce5bcfef7ed83414c1b
byendan <byendanb@gmail.com>
2016-08-12 14:06:41 -0700

    Rover model, and model tests

---
+++ app/models/rover_module.rb
@@ -0,0 +1,40 @@
+class RoverModule < ActiveRecord::Base
+  validates :rover, presence: true
+  validates :date, presence: true
+
+  def initialize(options={})
+    super
+    self.name = "Rover Pictures"
+    self.base_query = 'https://api.nasa.gov/mars-photos/api/v1/rovers/'
+  end
+
+  def build_query
+    return_query = self.base_query
+    return_query += "#{self.rover}/photos?"
+    return_query += "earth_date=#{self.date}&"
+    return_query += "camera=#{self.camera}&" if self.camera != nil
+
+    # Saves the full query for easier access
+    self.update_attributes(full_query: return_query)
+
+    return_query
+  end
+
+  def return_array(json_response)
+    pictures = []
+
+    if json_response["photos"] == nil
+      return "no data found"
+    else
+
+      json_response["photos"].each do |picture_hash|
+        pictures << picture_hash["img_src"]
+      end
+    end
+
+
+    return pictures
+  end
+
+  # TODO make a way to determine valid dates that can be used
+end
---
+++ spec/factories/rover_modules.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :rover_module do
+    
+  end
+
+end
