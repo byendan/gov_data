2369a1196d6a81bfe068c227858482904e73d69e
byendan <byendanb@gmail.com>
2016-08-16 22:18:09 -0700

    You will be informed if no data is found for a rover search

--- app/controllers/rover_modules_controller.rb
+++ app/controllers/rover_modules_controller.rb
@@ -8,8 +8,15 @@ class RoverModulesController < ApplicationController
   def create
     @rover_module = RoverModule.new(rover_params)
     @rover_module.date = fix_date(@rover_module.date)
-    @rover_module.save!
-    redirect_to @rover_module
+
+    if rover_request(@rover_module).class == Array
+      @rover_module.save
+      redirect_to @rover_module
+    else
+      flash[:error] = "No pictures for that Rover, Camera, Date combination"
+      redirect_to new_rover_module_path
+    end
+
   end
 
   def show
@@ -23,7 +30,7 @@ class RoverModulesController < ApplicationController
 
   def update
     @rover_module = RoverModule.find(params[:id])
-    @rover_module.update_attributes(params)
+    @rover_module.update_attributes(rover_params)
     redirect_to @rover_module
   end
 
@@ -57,5 +64,9 @@ class RoverModulesController < ApplicationController
       new_date = "#{date_ary[2]}-#{date_ary[1]}-#{date_ary[0]}"
 
       return new_date
+    end
+
+    def rover_request(rover_module)
+      return rover_module.return_array(make_request(rover_module.build_query))
     end
 end
